cfa2d6b3c8de84d080bb1f3a27b52cbea474f5da
taruna-wd <tarunarajoriya18@gmail.com>
2024-05-23 17:06:54 +0530

    geometry

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,14 @@
+mapboxgl.accessToken = maptoken  ;
 
-    mapboxgl.accessToken = maptoken  ;
+// mapboxgl.accessToken = 'pk.eyJ1IjoidGFydW5hMjIyIiwiYSI6ImNsd2hxNzJpeDBqMTYycW1td283cmZmaTcifQ.YZ_UjVrHE_CI5SHaa9E0JQ   '  ;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    center: coordinates, // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
+console.log(coordinates)
 
-	// mapboxgl.accessToken = 'pk.eyJ1IjoidGFydW5hMjIyIiwiYSI6ImNsd2hxNzJpeDBqMTYycW1td283cmZmaTcifQ.YZ_UjVrHE_CI5SHaa9E0JQ   '  ;
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        center: [coordinates], // starting position [lng, lat]
-        zoom: 9 // starting zoom
-    });
-    console.log(coordinates)
-
-  // Create a default Marker and add it to the map.
-  const marker = new mapboxgl.Marker()
-  .setLngLat([coordinates]) // Listing.geometry.coordinates 
-  .addTo(map);
+// Create a default Marker and add it to the map.
+const marker = new mapboxgl.Marker()
+.setLngLat(coordinates) // Listing.geometry.coordinates 
+.addTo(map);
